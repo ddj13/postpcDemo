82ca89484563d9e65ea8ebbf095c153abcc64967
Daniel <ddjs13@cs.huji.ac.il>
2019-05-24 16:33:18 +0300

    works perfect

--- app/src/main/java/com/selfChat/ex5/MainActivity.java
+++ app/src/main/java/com/selfChat/ex5/MainActivity.java
@@ -1,6 +1,7 @@
 package com.selfChat.ex5;
 
 import android.content.Intent;
+import android.os.Trace;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,7 +17,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        View textView=findViewById(R.id.tapText);
+        setClickable(textView);
+        View buttonView=findViewById(R.id.goFishBtn);
+        setClickable(buttonView);
+    }
+    private void setClickable(View view ){
+        if(!view.isClickable()){
+            view.setClickable(true);
+        }
+    }
     public void tapTextOnClick(View view) {
+        view.setClickable(false);
         Intent intent=new Intent(this,DigitsReadScreen.class);
         startActivityForResult(intent,1);
     }
@@ -33,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onGoFishClick(View view) {
+        view.setClickable(false);
         Intent intent=new Intent(this,FishingScreen.class);
         startActivity(intent);
     }
